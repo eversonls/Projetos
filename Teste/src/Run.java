0049a8412d24f8e949834ed61a9ad2129bb44f85
eversonls <everson.lisboa.santos@gmail.com>
2014-02-17 14:01:18 -0300

    teste de olá

---
+++ Teste/src/Run.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Everson
+ *
+ */
+public class Run {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Teste");
+
+	}
+
+}
---
+++ Teste/src/teste.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author Everson
+ *
+ */
+public class teste {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Pegou");
+		
+
+	}
+
+}
