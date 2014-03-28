765558dd7ec14d6a45cb5c9c42875550115e077e
Everson Lisboa <everson.lisboa.santos@gmail.com>
2014-03-28 17:04:33 -0300

    Ola

---
+++ Cliente/src/Cliente.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Everson Ebi
+ *
+ */
+public class Cliente {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
